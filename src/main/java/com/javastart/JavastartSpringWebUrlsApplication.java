25dfd119261165d35c5ddfa157225bc50467047c
Krzysztof <krzubrycki@gmail.com>
2022-03-08 13:36:17 +0100

    created project

---
+++ src/main/java/com/javastart/JavastartSpringWebUrlsApplication.java
@@ -0,0 +1,13 @@
+package com.javastart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavastartSpringWebUrlsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavastartSpringWebUrlsApplication.class, args);
+    }
+
+}
