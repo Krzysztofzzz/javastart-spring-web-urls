7f3b8840a4ed9656995cdb9af834fce22ae5ee51
Krzysztof <krzubrycki@gmail.com>
2022-03-08 14:07:13 +0100

    application to view imagest with relative urls

--- src/main/java/com/javastart/PictureRepository.java
+++ src/main/java/com/javastart/PictureRepository.java
@@ -1,4 +1,29 @@
 package com.javastart;
 
+import org.springframework.stereotype.Repository;
+
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+
+@Repository
 public class PictureRepository {
+    private final Map<String, Picture> pictures = new HashMap<>();
+
+    public PictureRepository() {
+        Picture birdPicture = new Picture("bird", "Biały ptak", "bird.jpeg");
+        Picture flowerPicture = new Picture("flower", "Żółty kwiatek", "flower.jpeg");
+        Picture handPicture = new Picture("hand", "Ręka pośród kwaitów", "hand.jpeg");
+        pictures.put(birdPicture.getId(), birdPicture);
+        pictures.put(flowerPicture.getId(), flowerPicture);
+        pictures.put(handPicture.getId(), handPicture);
+    }
+
+    Picture findImageById(String id) {
+        return pictures.get(id);
+    }
+
+    Collection<Picture> findAll() {
+        return pictures.values();
+    }
 }
